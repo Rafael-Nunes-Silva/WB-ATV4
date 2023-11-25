90d108bb7222267f6464b4ed17206c1199e3f25f
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2023-11-25 18:51:18 -0300

    Backend

---
+++ backend/WorldBeauty/GetProdutosHandler.java
@@ -0,0 +1,22 @@
+package WorldBeauty;
+
+import java.io.OutputStream;
+import java.io.IOException;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+public class GetProdutosHandler implements HttpHandler {
+    public void handle(HttpExchange he) throws IOException {
+        he.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+        he.getResponseHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        he.getResponseHeaders().add("Access-Control-Allow-Credentials", "true");
+        he.getResponseHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+
+        String response = "{produtos:" + Empresa.GetProdutos().toArray().toString() + "}";
+        he.sendResponseHeaders(200, response.length());
+
+        OutputStream os = he.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
+}
